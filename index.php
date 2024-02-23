c421353e1155b5e3a7960d7ef61cdc8d0fcb150f
Juan Ernesto Méndez Nerio <mn18015@ues.edu.sv>
2024-02-23 17:25:13 -0600

    materias

---
+++ index.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Materias</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body style="background-color:aliceblue" >
+    <section>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">CICLO I - 2024</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        
+                    </ul>
+                    
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <center>
+                    <div class="col-md-12">
+                        <h3>Clases Teoricas</h3>
+                    </div>
+                </center>
+
+            </div>
+            <center>
+                <div class="row ">
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="public/imagenes/dsi115.jpg" class="card-img-top" alt="..." width="200px" height="200px">
+                            <div class="card-body">
+                                <h5 class="card-title">DSI115</h5>
+                                <p class="card-text">Link para la clase teorica</p>
+                                <a href="https://meet.google.com/xdk-drgh-ukn?pli=1&authuser=1" class="btn btn-primary">Ir a la clase</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="public/imagenes/pdm115.png" class="card-img-top" alt="..." width="200px" height="200px">
+                            <div class="card-body">
+                                <h5 class="card-title">PDM115</h5>
+                                <p class="card-text">Link para las clases teoricas</p>
+                                <a href="https://meet.google.com/jdw-wqdh-ubv?pli=1&authuser=1" class="btn btn-primary">Lunes</a>
+
+                                <a href="https://meet.google.com/cjp-sssz-kxa?pli=1&authuser=1" class="btn btn-primary">Miercoles</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="public/imagenes/tad115.jpg" class="card-img-top" alt="..." width="200px" height="200px">
+                            <div class="card-body">
+                                <h5 class="card-title">TAD115</h5>
+                                <p class="card-text">Link para las clases teorica</p>
+                                <a href="https://meet.google.com/gbc-whzu-kkt?pli=1&authuser=1" class="btn btn-primary">Ir a la clase</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="public/imagenes/mip115" class="card-img-top" alt="..." width="200px" height="200px">
+                            <div class="card-body">
+                                <h5 class="card-title">MIP115</h5>
+                                <p class="card-text">Link para las clases teoricas</p>
+                                <a href="https://meet.google.com/wrn-sfkw-dfd?pli=1&authuser=1" class="btn btn-primary">Ir a la clase</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </center>
+        </div>
+    </section>
+
+    <section>
+        <br>
+        <div class="container-fluid">
+            <div class="row">
+                <center>
+                    <div class="col-md-12">
+                        <h3>Clases de Laboratorios</h3>
+                    </div>
+                </center>
+
+            </div>
+            <center>
+                <div class="row ">
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="public/imagenes/dsi115.jpg" class="card-img-top" alt="..." width="200px" height="200px">
+                            <div class="card-body">
+                                <h5 class="card-title">DSI115</h5>
+                                <p class="card-text">Link para la clase de laboratorio</p>
+                                <a href="#" class="btn btn-primary">Ir a la clase</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="public/imagenes/pdm115.png" class="card-img-top" alt="..." width="200px" height="200px">
+                            <div class="card-body">
+                                <h5 class="card-title">PDM115</h5>
+                                <p class="card-text">Link para la clase de laboratorio</p>
+                                <a href="https://meet.google.com/hxa-mvkw-tuy?pli=1&authuser=1" class="btn btn-primary">Ir a la clase</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="public/imagenes/tad115.jpg" class="card-img-top" alt="..." width="200px" height="200px">
+                            <div class="card-body">
+                                <h5 class="card-title">TAD115</h5>
+                                <p class="card-text">Link para las clase de laboratorio</p>
+                                <a href="#" class="btn btn-primary">Ir a la clase</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="public/imagenes/mip115" class="card-img-top" alt="..." width="200px" height="200px">
+                            <div class="card-body">
+                                <h5 class="card-title">MIP115</h5>
+                                <p class="card-text">Link para las clase de laboratorio</p>
+                                <a href="https://meet.google.com/hcb-jkgs-rvd?pli=1&authuser=1" class="btn btn-primary">Ir a la clase</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </center>
+        </div>
+        <br>
+        <br>
+    </section>
+    <footer class="bg-dark text-light py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p class="mb-1">Universidad de El Salvador</p>
+                    <p class="mb-1">Facultad de Ingeniería y Arquitectura</p>
+                    <p class="mb-1">Juan Ernesto Méndez Nerio</p>
+                    <hr class="bg-light" style="width: 50%; margin: 1rem auto;">
+                    <p class="mb-0">Dirección: 123 Calle Principal, San Salvador, El Salvador</p>
+                    <p class="mb-0">Teléfono: +503 6979-4062</p>
+                    <p class="mb-0">Correo electrónico: mn18015@ues.edu.sv</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script>
+        document.getElementById('searchInput').addEventListener('input', function() {
+            var searchQuery = this.value.trim().toLowerCase();
+            var items = document.querySelectorAll('#searchList li');
+
+            items.forEach(function(item) {
+                var text = item.textContent.trim().toLowerCase();
+                if (text.includes(searchQuery)) {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+    </script>
+
+</body>
+
+</html>
